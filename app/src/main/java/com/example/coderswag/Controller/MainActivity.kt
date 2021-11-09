f6ec964439e40e48850a43a36b1a6ee8d1a9915c
umerasghar24 <uasghar24@gmail.com>
2021-11-09 12:24:30 +0500

    prepare for recycler view adapter

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Adapter
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.coderswag.Adapters.CategoryAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
@@ -11,13 +13,14 @@ import com.example.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter : CategoryAdapter //using own costum adapter built in Adapter //adapter takes data from model and display on UI
+    lateinit var adapter: CategoryAdapter //using own costum adapter built in Adapter //adapter takes data from model and display on UI
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //we are giving info where to take data //initializing array adapter
-        adapter =CategoryAdapter(this,DataService.categories) //data services the object
+        adapter = CategoryAdapter(this, DataService.categories) //data services the object
         //tell the listview we created who it need listen to
-        categoryListView.adapter =adapter
+        categoryListView.adapter = adapter
+
     }
 }
